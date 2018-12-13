d0c4dd2f09260b93bf6c697c3c07f1bba61e68d9
guitarrapc <3856350+guitarrapc@users.noreply.github.com>
2018-12-13 17:51:17 +0900

    add(master) docker build files

---
+++ master/docker-build.bat
@@ -0,0 +1,3 @@
+docker image build -t go-python:1.0.0 -f Dockerfile.go-python .
+docker image build -t datadog-build-base-official:1.0.0 -f Dockerfile.build-base . 
+docker image build --no-cache -t datadog-build .
